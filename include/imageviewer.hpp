42e6975ca08b6bbbec3f00487fe9ff40a718ad05
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-05-07 15:11:22 -0300

    refactor: add docstrings

--- include/imageviewer.hpp
+++ include/imageviewer.hpp
@@ -13,28 +13,120 @@ class ImageViewer : public QMainWindow
 
 public:
     ImageViewer();
+
+    /**
+     * @brief 
+     * 
+     * @return true 
+     * @return false 
+     */
     bool loadFile(const QString &);
 
 private slots:
+
+    /**
+     * @brief Open an image
+     * 
+     */
     void open();
+
+    /**
+     * @brief Save the current image at the same opened file
+     * 
+     */
     void save();
+
+    /**
+     * @brief Opens a file dialog for saving the current image
+     * 
+     */
     void saveAs();
-    void print();
+
+    /**
+     * @brief Copy the current image to the clipboard
+     * 
+     */
     void copy();
+
+    /**
+     * @brief Paste the clipboard image, if any, to the viewer
+     * 
+     */
     void paste();
+
+    /**
+     * @brief Increases the aspect ratio to the viewing image
+     * 
+     */
     void zoomIn();
+
+    /**
+     * @brief Decreases the aspect ratio to the viewing image
+     * 
+     */
     void zoomOut();
+
+    /**
+     * @brief Return the current image to its original size
+     * 
+     */
     void normalSize();
+
+    /**
+     * @brief Resize the current image to fit the image viewing area
+     * 
+     */
     void fitToWindow();
+
+    /**
+     * @brief Open a dialog with information about the software
+     * 
+     */
     void about();
 
 private:
+
+    /**
+     * @brief Create all menu actions
+     * 
+     */
     void createActions();
-    void createMenus();
+
+    /**
+     * @brief 
+     * 
+     */
     void updateActions();
+
+    /**
+     * @brief Save a file
+     * 
+     * @param fileName Name of the file
+     * @return true 
+     * @return false 
+     */
     bool saveFile(const QString &fileName);
+
+    /**
+     * @brief Place the newImage into the image Label (area)
+     * 
+     * @param newImage 
+     */
     void setImage(const QImage &newImage);
+
+    /**
+     * @brief Redimension the image by using the factor
+     * 
+     * @param factor 
+     */
     void scaleImage(double factor);
+
+    /**
+     * @brief Move the scrollBar
+     * 
+     * @param scrollBar 
+     * @param factor 
+     */
     void adjustScrollBar(QScrollBar *scrollBar, double factor);
 
     QImage image;
@@ -43,13 +135,8 @@ private:
     QScrollArea *scrollArea;
     double scaleFactor;
 
-#ifndef QT_NO_PRINTER
-    QPrinter printer;
-#endif
-
     QAction *saveAct;
     QAction *saveAsAct;
-    QAction *printAct;
     QAction *copyAct;
     QAction *zoomInAct;
     QAction *zoomOutAct;
