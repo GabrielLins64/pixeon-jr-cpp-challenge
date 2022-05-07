88fc0e53f6362425e08c38b1e6f116bbc4e99bd4
Gabriel Lins <gabrielinsmelo@gmail.com>
2022-05-07 13:32:07 -0300

    feat: add Qt Image Viewer example

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
+#include <QApplication>
+#include <QCommandLineParser>
 
-int main(int argc, char const *argv[])
+#include "imageviewer.hpp"
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello, world!" << std::endl;
-
-    return 0;
+    QApplication app(argc, argv);
+    QGuiApplication::setApplicationDisplayName(ImageViewer::tr("Image Viewer"));
+    QCommandLineParser commandLineParser;
+    commandLineParser.addHelpOption();
+    commandLineParser.addPositionalArgument(ImageViewer::tr("[file]"), ImageViewer::tr("Image file to open."));
+    commandLineParser.process(QCoreApplication::arguments());
+    ImageViewer imageViewer;
+    if (!commandLineParser.positionalArguments().isEmpty()
+        && !imageViewer.loadFile(commandLineParser.positionalArguments().front()))
+    {
+        return -1;
+    }
+    imageViewer.show();
+    return app.exec();
 }
